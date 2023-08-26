50d11b99e79c45e534753a731aa870daa4427a96
MOZGIII <mike-n@narod.ru>
2023-08-26 19:58:24 -0300

    Implement backend

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
 pub mod config;
 
-#[derive(Debug, Clone, enum_kinds::EnumKind)]
+#[derive(Debug, Clone, Default, enum_kinds::EnumKind)]
 #[enum_kind(
     BackendKind,
     cfg_attr(feature = "clap", derive(clap::ValueEnum)),
     cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))
 )]
 pub enum Backend<Config: self::Config> {
+    /// A dummy backend that panics on any operation.
+    /// This is required because the [`burn_tensor::backend::Backend`] trait
+    /// requires a [`Default`] to be implemented.
+    #[default]
+    Panic,
+
     #[cfg(feature = "burn-tch")]
     Tch(burn_tch::TchBackend<Config::TchElement>),
     #[cfg(feature = "burn-wgpu")]
@@ -34,4 +40,30 @@ pub trait Config {
 
     #[cfg(feature = "burn-ndarray")]
     type NdarrayElement: burn_ndarray::FloatNdArrayElement;
+}
+
+impl<Config: self::Config> burn_tensor::backend::Backend for Backend<Config> {
+    type Device;
+
+    type FullPrecisionBackend;
+
+    type FullPrecisionElem;
+
+    type TensorPrimitive<const D: usize>;
+
+    type FloatElem;
+
+    type IntTensorPrimitive<const D: usize>;
+
+    type IntElem;
+
+    type BoolTensorPrimitive<const D: usize>;
+
+    fn name() -> String {
+        todo!()
+    }
+
+    fn seed(seed: u64) {
+        todo!()
+    }
 }
